0af4de33038c2cb9cecd506569a06665864ae206
Muhammad Rendra <mnrendra@users.noreply.github.com>
2025-05-19 19:47:05 +0700

    feat: git

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,10 @@
 echo "Hello $1"
 time=$(date)
 echo "time=$time" >> $GITHUB_OUTPUT
+
 ls -laihs
+cat entrypoint.sh
+
+git config --global --add safe.directory /github/workspace
 git config --list
 git log
