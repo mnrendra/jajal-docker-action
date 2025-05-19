51848f6e3c86516e45799fd37d105b52a642af11
Muhammad Rendra <mnrendra@users.noreply.github.com>
2025-05-20 05:37:14 +0700

    feat: passphrase

--- src/scripts/importgpg.sh
+++ src/scripts/importgpg.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 IFS=$'\n\t'
 echo "$GPG_PRIVATE_KEY" > private.key
 if [ -n "$GPG_PASSPHRASE" ]; then
-  echo "$GPG_PASSPHRASE" | gpg --batch --yes --passphrase-fd 0 --import private.key
+  echo "$GPG_PASSPHRASE" | gpg --batch --yes --passphrase-fd 0 --pinentry-mode loopback --import private.key
 else
   gpg --batch --yes --import private.key
 fi
@@ -18,3 +18,17 @@ git config --global user.name "${GIT_AUTHOR_NAME:-$DEFAULT_GIT_AUTHOR_NAME}"
 git config --global user.email "${GIT_AUTHOR_EMAIL:-$DEFAULT_GIT_AUTHOR_EMAIL}"
 git config --global user.signingkey "$KEY_ID"
 git config --global gpg.program gpg
+
+add_unique_line() {
+  local line="$1"
+  local file="$2"
+  grep -qxF "$line" "$file" || echo "$line" >> "$file"
+}
+
+mkdir -p ~/.gnupg
+
+add_unique_line "use-agent" ~/.gnupg/gpg.conf
+add_unique_line "pinentry-mode loopback" ~/.gnupg/gpg.conf
+add_unique_line "allow-loopback-pinentry" ~/.gnupg/gpg-agent.conf
+
+chmod 600 ~/.gnupg/*
