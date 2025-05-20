502bf7d05413bb935c8757af4f5f13a9258f5cde
Muhammad Rendra <mnrendra@users.noreply.github.com>
2025-05-20 15:24:52 +0700

    feat: move core

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import type { Result } from 'semantic-release'
 
 import { chdir } from 'node:process'
 
+import { getInput } from '@actions/core'
+
 import semanticRelease from 'semantic-release'
 
 import { WORKSPACE_DIR } from './consts'
@@ -14,6 +16,22 @@ const main = async (
   //
 ): Promise<Result> => {
   chdir(WORKSPACE_DIR)
+
+  const gitAuthorName = getInput('git-author-name')
+  const gitAuthorEmail = getInput('git-author-email')
+
+  const gitCommitterName = getInput('git-committer-name')
+  const gitCommitterEmail = getInput('git-committer-email')
+
+  const gpgPassphrase = getInput('gpg-passphrase')
+
+  console.log('gitAuthorName:', gitAuthorName, 'env:', process.env['INPUT_GIT-AUTHOR-NAME'])
+  console.log('gitAuthorEmail:', gitAuthorEmail, 'env:', process.env['INPUT_GIT-AUTHOR-EMAIL'])
+
+  console.log('gitCommitterName:', gitCommitterName, 'env:', process.env['INPUT_GIT-COMMITTER-NAME'])
+  console.log('gitCommitterEmail:', gitCommitterEmail, 'env:', process.env['INPUT_GIT-COMMITTER-NAME'])
+
+  console.log('gpgPassphrase:', gpgPassphrase, 'env:', process.env['INPUT_GIT-PASSPHRASE'])
 
   await execCmd(`git config --global --add safe.directory ${WORKSPACE_DIR}`)
 
--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,29 +1,11 @@
 import { argv } from 'node:process'
 
-import { getInput } from '@actions/core'
-
 import { execCmd } from '../utils'
 
 const main = async (): Promise<string> => {
   const [version, notes, branch] = argv.slice(2)
 
   const tag = `v${version}`
-
-  const gitAuthorName = getInput('git-author-name')
-  const gitAuthorEmail = getInput('git-author-email')
-
-  const gitCommitterName = getInput('git-committer-name')
-  const gitCommitterEmail = getInput('git-committer-email')
-
-  const gpgPassphrase = getInput('gpg-passphrase')
-
-  console.log('gitAuthorName:', gitAuthorName)
-  console.log('gitAuthorEmail:', gitAuthorEmail)
-
-  console.log('gitCommitterName:', gitCommitterName)
-  console.log('gitCommitterEmail:', gitCommitterEmail)
-
-  console.log('gpgPassphrase:', gpgPassphrase)
 
   await execCmd(`git tag -d ${tag}`)
 
