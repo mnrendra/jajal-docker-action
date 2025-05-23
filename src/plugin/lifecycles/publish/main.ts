98565c3ddfc8c9ddc07e2b1dfb4c427abebe5243
Muhammad Rendra <mnrendra@users.noreply.github.com>
2025-05-24 00:57:59 +0700

    feat: plugin

--- src/plugin/lifecycles/publish/main.ts
+++ src/plugin/lifecycles/publish/main.ts
@@ -1,8 +1,10 @@
-import type { PublishContext } from 'semantic-release'
+import type { Options as SROptions, PublishContext } from 'semantic-release'
+
+import { execCmd } from '../../../utils'
 
 import { parseContext } from './utils'
 
-interface Options {
+interface Options extends SROptions {
   name?: string
 }
 
@@ -18,6 +20,33 @@ const main = async (
   console.log('version:', version)
   console.log('tag:', tag)
   console.log('notes:', notes)
+
+  console.log('-------------- delete local tag ----------')
+  const a = await execCmd(`git tag -d ${tag}`)
+  console.log('stderr:', a.stderr)
+  console.log('stdout:', a.stdout)
+
+  console.log('-------------- delete remote tag ---------')
+  const b = await execCmd(`git push origin -d tag ${tag}`)
+  console.log('stderr:', b.stderr)
+  console.log('stdout:', b.stdout)
+
+  const message = `release: ${tag}`
+
+  console.log('-------------- commit release ------------')
+  const c = await execCmd(`git commit -S --allow-empty -m "${message}\n\n${notes}"`)
+  console.log('stderr:', c.stderr)
+  console.log('stdout:', c.stdout)
+
+  console.log('-------------- tag release ---------------')
+  const d = await execCmd(`git tag -s ${tag} -m "${message}\n\n${notes}"`)
+  console.log('stderr:', d.stderr)
+  console.log('stdout:', d.stdout)
+
+  console.log('-------------- push release --------------')
+  const e = await execCmd('git push origin --follow-tags')
+  console.log('stderr:', e.stderr)
+  console.log('stdout:', e.stdout)
 }
 
 export default main
--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,9 +1,13 @@
 import { argv } from 'node:process'
 
-import { execCmd } from '../utils'
+// import { execCmd } from '../utils'
 
 const main = async (): Promise<string> => {
-  const [version, notes, branch] = argv.slice(2)
+  const [
+    version,
+    // notes,
+    branch
+  ] = argv.slice(2)
 
   if (
     typeof version !== 'string' ||
@@ -14,33 +18,33 @@ const main = async (): Promise<string> => {
     return branch
   }
 
-  const tag = `v${version}`
-  const message = `release: ${tag}`
+  // const tag = `v${version}`
+  // const message = `release: ${tag}`
 
-  console.log('-------------- delete local tag ----------')
-  const a = await execCmd(`git tag -d ${tag}`)
-  console.log('stderr:', a.stderr)
-  console.log('stdout:', a.stdout)
+  // console.log('-------------- delete local tag ----------')
+  // const a = await execCmd(`git tag -d ${tag}`)
+  // console.log('stderr:', a.stderr)
+  // console.log('stdout:', a.stdout)
 
-  console.log('-------------- delete remote tag ---------')
-  const b = await execCmd(`git push origin -d tag ${tag}`)
-  console.log('stderr:', b.stderr)
-  console.log('stdout:', b.stdout)
+  // console.log('-------------- delete remote tag ---------')
+  // const b = await execCmd(`git push origin -d tag ${tag}`)
+  // console.log('stderr:', b.stderr)
+  // console.log('stdout:', b.stdout)
 
-  console.log('-------------- commit release ------------')
-  const c = await execCmd(`git commit -S --allow-empty -m "${message}\n\n${notes}"`)
-  console.log('stderr:', c.stderr)
-  console.log('stdout:', c.stdout)
+  // console.log('-------------- commit release ------------')
+  // const c = await execCmd(`git commit -S --allow-empty -m "${message}\n\n${notes}"`)
+  // console.log('stderr:', c.stderr)
+  // console.log('stdout:', c.stdout)
 
-  console.log('-------------- tag release ---------------')
-  const d = await execCmd(`git tag -s ${tag} -m "${message}\n\n${notes}"`)
-  console.log('stderr:', d.stderr)
-  console.log('stdout:', d.stdout)
+  // console.log('-------------- tag release ---------------')
+  // const d = await execCmd(`git tag -s ${tag} -m "${message}\n\n${notes}"`)
+  // console.log('stderr:', d.stderr)
+  // console.log('stdout:', d.stdout)
 
-  console.log('-------------- push release --------------')
-  const e = await execCmd('git push origin --follow-tags')
-  console.log('stderr:', e.stderr)
-  console.log('stdout:', e.stdout)
+  // console.log('-------------- push release --------------')
+  // const e = await execCmd('git push origin --follow-tags')
+  // console.log('stderr:', e.stderr)
+  // console.log('stdout:', e.stdout)
 
   return branch
 }
