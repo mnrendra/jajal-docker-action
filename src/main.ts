7218f48d29253bd009e254def88317b61318d030
Muhammad Rendra <mnrendra@users.noreply.github.com>
2025-05-23 22:33:09 +0700

    fix: github token

--- src/main.ts
+++ src/main.ts
@@ -39,7 +39,8 @@ const main = async (): Promise<void> => {
     gitSignUser,
     gitSignCommit,
     gitSignTag,
-    gitSignPush
+    gitSignPush,
+    token
   } = getInputs()
 
   // console.log(cwd())
@@ -80,7 +81,8 @@ const main = async (): Promise<void> => {
       GIT_AUTHOR_NAME: name,
       GIT_AUTHOR_EMAIL: email,
       GIT_COMMITTER_NAME: name,
-      GIT_COMMITTER_EMAIL: email
+      GIT_COMMITTER_EMAIL: email,
+      GITHUB_TOKEN: token
     }
   })
 
