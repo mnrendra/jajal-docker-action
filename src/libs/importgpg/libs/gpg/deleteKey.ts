5b64cf95e7de5c7d13b76cb25f1dd23cef2db397
Muhammad Rendra <mnrendra@users.noreply.github.com>
2025-05-23 18:59:37 +0700

    fix: delete key

--- src/libs/importgpg/libs/gpg/deleteKey.ts
+++ src/libs/importgpg/libs/gpg/deleteKey.ts
@@ -3,20 +3,25 @@ import execGpg from './execGpg'
 const deleteKey = async (
   fingerprint: string
 ): Promise<void> => {
-  const args = [
-    '--batch',
-    '--yes',
-    fingerprint
-  ]
+  // const args = [
+  //   '--batch',
+  //   '--yes',
+  //   fingerprint
+  // ]
 
-  const setArgs = (
-    arg: string
-  ): string[] => {
-    return args.splice(args.length - 1, 0, arg)
-  }
+  // const setArgs = (
+  //   arg: string
+  // ): string[] => {
+  //   return args.splice(args.length - 1, 0, arg)
+  // }
 
-  await execGpg(setArgs('--delete-secret-keys'))
-  await execGpg(setArgs('--delete-keys'))
+  const a = await execGpg(['--batch', '--yes', '--delete-secret-keys', fingerprint])
+  console.log('stderr:', a.stderr)
+  console.log('stdout:', a.stdout)
+
+  const b = await execGpg(['--batch', '--yes', '--delete-keys', fingerprint])
+  console.log('stderr:', b.stderr)
+  console.log('stdout:', b.stdout)
 }
 
 export default deleteKey
