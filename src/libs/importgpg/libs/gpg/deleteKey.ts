0674c7c2c15089a2cbe017cd03bc3501179b2a83
Muhammad Rendra <mnrendra@users.noreply.github.com>
2025-05-23 19:19:08 +0700

    feat: delete key

--- src/libs/importgpg/libs/gpg/deleteKey.ts
+++ src/libs/importgpg/libs/gpg/deleteKey.ts
@@ -3,25 +3,22 @@ import execGpg from './execGpg'
 const deleteKey = async (
   fingerprint: string
 ): Promise<void> => {
-  // const args = [
-  //   '--batch',
-  //   '--yes',
-  //   fingerprint
-  // ]
+  const args = [
+    '--batch',
+    '--yes',
+    fingerprint
+  ]
 
-  // const setArgs = (
-  //   arg: string
-  // ): string[] => {
-  //   return args.splice(args.length - 1, 0, arg)
-  // }
+  const setArgs = (
+    arg: string
+  ): string[] => [
+    ...args.slice(0, 2),
+    arg,
+    ...args.slice(-1)
+  ]
 
-  const a = await execGpg(['--batch', '--yes', '--delete-secret-keys', fingerprint])
-  console.log('stderr:', a.stderr)
-  console.log('stdout:', a.stdout)
-
-  const b = await execGpg(['--batch', '--yes', '--delete-keys', fingerprint])
-  console.log('stderr:', b.stderr)
-  console.log('stdout:', b.stdout)
+  await execGpg(setArgs('--delete-secret-keys'))
+  await execGpg(setArgs('--delete-keys'))
 }
 
 export default deleteKey
--- src/main.ts
+++ src/main.ts
@@ -87,7 +87,7 @@ const main = async (): Promise<void> => {
   await execCmd('echo "tes" >> tes.txt')
   await execCmd('git add .')
   await execCmd('git commit -S -m "chore: add tes file"')
-  await execCmd('git tag -s v4.2.3-tes -m "release: v4.2.3-tes"')
+  await execCmd('git tag -s v4.3.0-tes -m "release: v4.3.0-tes"')
   await execCmd('git push --follow-tags')
 
   await cleanupGPG(digest)
