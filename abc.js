f8e198818a25bc59550f0e7e7f715c9ba50fa580
Muhammad Rendra <mnrendra@users.noreply.github.com>
2025-05-26 17:08:55 +0700

    feat: add abc

--- abc.js
+++ abc.js
@@ -1,35 +1,3 @@
-const context = {
-  nextRelease: {
-    version: '1.2.3',
-    notes: 'hallo\n\nworld!'
-  },
-  branch: {
-    name: 'main',
-    meta: {
-      name: 'susu'
-    }
-  }
-}
+const { existsSync } = require('node:fs')
 
-console.log(context['nextRelease.version'])
-
-const str = 'release: {branch["meta"].name} v{nextRelease["version"]}\n\n{nextRelease.notes}\n\non branch: {branch.name}'
-
-const matches = [...str.matchAll(/{(.*?)}/g)].map(m => m[1])
-
-const getNested = (object, path) => {
-  return path.split('.').reduce((acc, key) => acc?.[key], object)
-}
-
-let msg = str
-
-console.log('matches:', matches)
-
-matches.forEach((key) => {
-  console.log('key:', key)
-  const val = getNested(context, key)
-  console.log('val:', val)
-  msg = msg.replace(`{${key}}`, val)
-})
-
-console.log(msg)
+console.log(existsSync('action.yml'))
